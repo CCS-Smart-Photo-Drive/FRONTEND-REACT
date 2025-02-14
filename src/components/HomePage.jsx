4b002a0c57186ba3d4263d83eec6cf24f3a5e7f0
Hari21-Tech <sri21harisesh@gmail.com>
2025-02-14 18:00:01 +0530

    bruh

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -11,7 +11,7 @@ const FuturisticHome = () => {
 
   const clientId = '679a6500b3cee642388a3e77'; 
   const handleLogin = () => {
-    const authUrl = `https://auth.ccstiet.com/auth/google?clientid=${clientId}&callback=${window.location.href}/verify_login`;
+    const authUrl = `https://auth.ccstiet.com/auth/google?clientid=${clientId}&callback=${window.location.href}verify_login`;
     window.location.href = authUrl;
   };
 
