5531cc7b522589ab37b1a7a7d84e76072c271264
Hari21-Tech <sri21harisesh@gmail.com>
2025-02-27 14:31:56 +0530

    Somce changes

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const IS_DEV = import.meta.env.MODE === 'development';
 
 //todo put api here
-export const API_URL = (domain) => IS_DEV ? 'http://localhost:5000' :'https://api-smartdrive.ccstiet.com'
+export const API_URL = (domain) => IS_DEV ? 'https://api-smartdrive.ccstiet.com' :'https://api-smartdrive.ccstiet.com'
 
 
